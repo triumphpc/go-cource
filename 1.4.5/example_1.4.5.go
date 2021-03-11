905a6b940f3a61517e3362f900238c1f0906f0c9
Sergey Vrulin <triumph.job@gmail.com>
2021-03-11 21:13:18 +0300

    first update

---
+++ 1.4.5/example_1.4.5.go
@@ -0,0 +1,16 @@
+/**
+Go (Golang) From simple to great. The Complete Developer's Guide.
+Example 1.4.5.
+
+@author Alex Versus 2021
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("This is our first program\n")
+}
