70cf4434193bbbca2ae8aec88b719ab0e49f1c33
Sergey Vrulin <triumph.job@gmail.com>
2021-03-23 21:52:03 +0300

    add gitinore

---
+++ 2.22/Arithmetic/7.go
@@ -0,0 +1,13 @@
+/**
+Go (Golang) From simple to great. The Complete Developer's Guide.
+
+1. Write Celsius to Fahrenheit Temperature Converter
+
+@author Alex Versus 2021
+*/
+
+package main
+
+func main() {
+
+}
